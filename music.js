3d4b638e379b00d2cc7d5e5bbd650fe0cfd665ea
Jatin <thejatinvaghela@gmail.com>
2022-08-17 21:12:02 +0530

    a

--- music.js
+++ music.js
@@ -1,3 +1,6 @@
+let form = document.querySelector("form").addEventListener("submit", (e)=>{e.preventDefault();})
+
+
 
 let Submit_Search_value = document.getElementById("Hight2");
 Submit_Search_value.addEventListener("click", getSearch_Value);
@@ -8,6 +11,9 @@ let Song_Name_History=[];
 function getSearch_Value() {
 
     let Search_value = document.getElementById("Hight");
+    if () {
+        
+    }
     Song_Name = Search_value.value;
     Search_History={ 
         Song_Name,
@@ -17,4 +23,21 @@ function getSearch_Value() {
     
     console.log(Song_Name);
     console.log(Song_Name_History);
+    Chack_L_S();
 };
+
+function Chack_L_S() {
+  let History = localStorage.getItem("History");
+  let History_Arry;
+  if (History===null) {
+    History_Arry=[];
+  } else {
+    History_Arry = JSON.parse(History);
+  }
+  History_Arry.push(...Song_Name_History);
+  
+  localStorage.setItem("History", JSON.stringify(History_Arry));
+
+  
+};
+
