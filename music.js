9422e6a518cfa6202fba0793b128610693a34fea
Jatin <thejatinvaghela@gmail.com>
2022-08-19 22:06:11 +0530

    changed Music Api

--- music.js
+++ music.js
@@ -60,7 +60,7 @@ function getSearch_Value() {
     
     Music_Api(Song_Name)
     console.log(URL_LINK);
-    URL_LINK.splice(0, 49)
+    URL_LINK.splice(0, 50)
     setTimeout(() => {
       console.log("HOME_CARD");
       
@@ -92,44 +92,50 @@ function Chack_L_S() {
 //=====================// Music    //tracks
 function Music_Api(Value) {
   
-  
+  if (Value===undefined) {
+    Value="fun";
+  } else {
+    Value=Value;
+  }
 
 console.log(Value);
 const Music = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': '80878a924amshac9807be2633669p16bf90jsn7b32fc7488bc',
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
   }
 };
-fetch(`https://spotify23.p.rapidapi.com/search/?q=${Value}&type=multi&offset=0&limit=50&numberOfTopResults=5`, Music)
+// fetch(`https://spotify23.p.rapidapi.com/search/?q=${Value}&type=multi&offset=0&limit=50&numberOfTopResults=5`, Music)
+fetch(`https://spotify81.p.rapidapi.com/search?q=${Value}&type=multi&offset=0&limit=50&numberOfTopResults=5`, Music)
   .then(response => response.json())
   .then(response => {
-           // console.log(response);
+            //console.log(response);
                 // console.log(response.tracks);
                 for (let key in response.tracks) {
                   let TRACKS= response.tracks[key] ;
-
+                  
                   for (let key2 in TRACKS) {
 
                    let TRACKS2 = TRACKS[key2]; 
-                       for (let key3 in TRACKS2) {
-                        //console.log(TRACKS2[key3]);
-                           let artist_Arry=(TRACKS2[key3].artists["items"]);
-                           let album_Arry=(TRACKS2[key3].albumOfTrack["name"]);
-                           let song_Duration_Milliseconds=(TRACKS2[key3].duration["totalMilliseconds"]);
+                    console.log(TRACKS2);
+                     // for (let key3 in TRACKS2) {
+                        console.log(TRACKS2);
+                           let artist_Arry=(TRACKS2.artists["items"]);
+                           let album_Arry=(TRACKS2.albumOfTrack["name"]);
+                           let song_Duration_Milliseconds=(TRACKS2.duration["totalMilliseconds"]);
                            let song_Duration_Minutes = Number((song_Duration_Milliseconds / 60000).toFixed(2));
-                           let song_album_cover = (TRACKS2[key3].albumOfTrack["coverArt"].sources[1]);
+                           let song_album_cover = (TRACKS2.albumOfTrack["coverArt"].sources[1]);
                            
                           
                              let SONGS={ 
-                                      SONG_LINK: TRACKS2[key3].uri,
-                                      SONG_NAME: TRACKS2[key3].name,
+                                      SONG_LINK: TRACKS2.uri,
+                                      SONG_NAME: TRACKS2.name,
                                       SONG_DURATION: song_Duration_Minutes,
                                       SONG_ARTIST_LINK: artist_Arry[0].uri,
                                       SONG_ARTIST_NAME:artist_Arry[0].profile["name"],
-                                      SONG_ALBUM_LINK: TRACKS2[key3].albumOfTrack["uri"],
-                                      SONG_ALBUM_NAME: TRACKS2[key3].albumOfTrack["name"],
+                                      SONG_ALBUM_LINK: TRACKS2.albumOfTrack["uri"],
+                                      SONG_ALBUM_NAME: TRACKS2.albumOfTrack["name"],
                                       SONG_ALBUM_COVER: song_album_cover,
                                     }
                                   
@@ -137,14 +143,14 @@ fetch(`https://spotify23.p.rapidapi.com/search/?q=${Value}&type=multi&offset=0&l
                                     
                               
                           
-                       }
+                      // }
 
-                  }
+                  } 
 
                 }
 
    }
-   )
+  )
 
 //=====================// END Music                           
 
